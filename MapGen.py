9a6254070909b8bb5ee41e53a971aa96e4fd5890
sophianova <sophianova@auth.cs.nmt.edu>
2021-03-12 14:41:13 -0700

    Generates map of data from map_point objects.

---
+++ MapGen.py
@@ -0,0 +1,36 @@
+# @author Sophia Novo-Gradac
+# 3/12/2021
+# @brief:   places circles on canvas based on map_point objects and displays canvas.
+
+from tkinter import *
+import MapPoint
+import gpsPoint
+
+canvas_size = 500 #test canvas size
+
+"""tester for making drone path"""
+def create_circle(map_point, canvasName): #center coordinates, radius
+    x = map_point.x
+    y = map_point.y
+    return canvasName.create_oval(x - 1, y - 1, x + 1, y + 1)
+
+#test on sophia's pc
+gps_points = gpsPoint.read_drone_csv("/home/nova/cse326/Data Files_Feb-26th-2021-05-57PM-Flight-Airdata.csv")
+map_points = MapPoint.set_xy(gps_points)
+
+window = Tk()
+map = Canvas(window,
+            width = canvas_size,
+            height = canvas_size)
+map.pack()
+
+count = -1
+while True:
+    count += 1
+
+    if count >= len(map_points):
+        break
+
+    create_circle(map_points[count], map)
+
+window.mainloop()

43f1cd897de79a3cc47964d3885a5faf085745b3
sophianova <sophianova@auth.cs.nmt.edu>
2021-03-16 14:31:47 -0600

    circles now have a color fill determined by stella_point

--- MapGen.py
+++ MapGen.py
@@ -1,41 +1,41 @@
 # @author Sophia Novo-Gradac
 # 3/12/2021
 # @brief:   places circles on canvas based on map_point objects and displays canvas.
+# last updated: 3/16/2021 by Sophia Novo-Gradac
+# @updates  added color and radius to create_circle
+#           black is set as map background
+# TODO:     Add maps for nir and vs
+#           export canvas as jpg?
 
 from tkinter import *
 import MapPoint
+import StellaPoint
 import gpsPoint
 
 canvas_size = 500 #test canvas size
 
 """tester for making drone path"""
-def create_circle(map_point, canvasName): #center coordinates, radius
+def create_circle(map_point, radius, canvasName): #center coordinates, radius
     x = map_point.x
     y = map_point.y
-    return canvasName.create_oval(x - 1, y - 1, x + 1, y + 1)
+    return canvasName.create_oval(x - radius, y - radius, x + radius, y + radius, fill = map_point.vs_rgb)
 
 #test on sophia's pc
-# gps_points = gpsPoint.read_drone_csv("/home/nova/cse326/Data Files_Feb-26th-2021-05-57PM-Flight-Airdata.csv")
 gps_points = gpsPoint.read_drone_csv("Data Files/Feb-26th-2021-05-57PM-Flight-Airdata.csv")
-map_points = MapPoint.set_xy(gps_points)
+stella_points = StellaPoint.make_stella_points("Data Files/data.csv")
+stella_points = StellaPoint.get_batch(stella_points, "1.X")
+map_points = MapPoint.set_xy(gps_points, stella_points)
 
 
 window = Tk()
 map = Canvas(window,
              width=canvas_size,
-             height=canvas_size)
+             height=canvas_size,
+             background = MapPoint.black)
 map.pack()
 
 for point in map_points:
-    create_circle(point, map)
-
-# count = -1
-# while True:
-#     count += 1
-#
-#     if count >= len(map_points):
-#         break
-#
-#     create_circle(map_points[count], map)
+    # point.print_point()
+    create_circle(point, 10, map)
 
 window.mainloop()

2addbd6690b320935f8b0ca743934738514297f2
tenise <tenise_stansfield@cs.nmt.edu>
2021-03-29 23:38:14 -0600

    implemented about() in menubar.py, just as a test run before implementing change_map.py - Tenise

--- menubar.py
+++ menubar.py
@@ -1,31 +1,38 @@
 # @author Tenise and Brynn
 # @date 3/26/21
 # @brief Menubar for the top of the screen / following Menubutton widget tutorial from Geeks4Geeks
-# @todo positoning, and buttons relating to each initial menu button
+# @todo commands
 from tkinter import *
 
 
 # this is the main function
 def menubar(root):
+    menu = Menu(root, background='black', foreground='white',
+                activebackground='grey', activeforeground='white')
 
-    menu = Menu(root)
-
-    filebutt = Menu(menu, tearoff=0)
+    # File
+    filebutt = Menu(menu, tearoff=0, background='black', foreground='white')
     filebutt.add_command(label="Open", command=open_file)
     filebutt.add_command(labe="Save", command=save_file)
-    filebutt.add_separator()
     menu.add_cascade(label="File", menu=filebutt)
 
-    viewbutt = Menu(menu,tearoff=0)
+    # View
+    viewbutt = Menu(menu, tearoff=0, background='black', foreground='white')
+    viewbutt.add_command(label="Infrared Map", command=change_map)
+    viewbutt.add_command(label="Near Infrared Map", command=change_map)
+    viewbutt.add_command(label="Other", command=change_map)
+    viewbutt.add_separator()
     viewbutt.add_command(label="Zoom In", command=zoom)
     viewbutt.add_command(label="Zoom Out", command=zoom)
     menu.add_cascade(label="View", menu=viewbutt)
 
-    annotatebutt = Menu(menu, tearoff=0)
+    # Annotate
+    annotatebutt = Menu(menu, tearoff=0, background='black', foreground='white')
     annotatebutt.add_command(label="New Annotation", command=new_annotate)
     menu.add_cascade(label="Annotate", menu=annotatebutt)
 
-    helpbutt = Menu(menu, tearoff=0)
+    # Help
+    helpbutt = Menu(menu, tearoff=0, background='black', foreground='white')
     helpbutt.add_command(label="About", command=about)
     menu.add_cascade(label="Help", menu=helpbutt)
 
@@ -37,17 +44,35 @@ def menubar(root):
 def open_file():
     x = 0
 
+
 def save_file():
     x = 0
+
 
 def zoom():
     x = 0
 
+
 def new_annotate():
     x = 0
 
+
 def about():
+    aboutWindow = Tk()
+    aboutWindow.title("About")
+    aboutWindow.geometry("400x300")
+
+    Label(aboutWindow,
+          text="Our team proposes to build an accurate visible and NIR (near-Infrared light) spectrum (or\n "
+               "Color-Infrared) mapping software specifically for STELLA (Science and Technology Education for\n "
+               "Land/Life Assessment), that will cartographically include and/or display STELLA’s other sensor\n "
+               "readings in a user friendly and visually appealing GUI (Graphical User Interface).\n "
+          ).pack()
+
+
+def change_map():
     x = 0
+
 
 if __name__ == '__main__':
     menubar()

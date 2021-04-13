702f392574cd34ecd36b8c52b62667e243211236
sophianova <sophianova@auth.cs.nmt.edu>
2021-04-13 10:05:53 -0600

    Added generic map generation functions Added function that draws map including altitude data

--- MapGen.py
+++ MapGen.py
@@ -4,18 +4,13 @@
 #           this is accomplished with triangles
 # last updated: 3/28/2021 by Sophia Novo-Gradac
 # @updates  fills entire map. done with triangles
-# TODO:     draw_data is computationally taxing, make a better algorithm
-#           export canvas as jpg?
-#           colors are consistently blended to the top right
-#           add method to determine which color from map_point to use
-#           canvas_size and resolution should be modifiable by user
-#           streamline imports. Many extra are included for testing purposes
+#           OR draw map using circles. radius calculated using data confidence
+# TODO:     fill map?
+#           determine aesthetics
 
 import tkinter as tk
 import MapPoint
-import StellaPoint
-import gpsPoint
-import Color
+import Color as col
 import math
 
 canvas_size = 1000 #test canvas size
@@ -91,10 +86,10 @@ class Triangle:
         if map_point.x > x_min and map_point.x < x_max:
             if map_point.y > y_min and map_point.y < y_max:
                 if mode == 'vis':
-                    self.set_color(map_point.vs_rgb)
+                    self.set_color(map_point.vis_rgb)
                 if mode == 'nir':
                     self.set_color(map_point.nir_rgb)
-                if mode == 'tmp':
+                if mode == 'temp':
                     self.set_color(map_point.temp_rgb)
                 return True
         return False
@@ -127,9 +122,9 @@ def clear_poly(poly_fill):
 
 '''find all triangles containing a map point and fill them
 add to list of filled triangles (this is used later!)'''
-def draw_data(map_points, poly_fill, mode, resolution, width):
+def draw_data(map_list, poly_fill, mode, resolution, width):
     filled = []
-    for mp in map_points: 
+    for mp in map_list: 
         # O(n^2) alg. Only use if other alg is failing
         # for t in poly_fill:
         #     if t.contains_point(mp, mode):
@@ -159,68 +154,114 @@ def fill_all(filled, poly_fill, width, height, resolution):
                     if poly_fill[j].color != "None":
                         colors.append(poly_fill[j].color)
                 if len(colors) > 0:
-                    poly_fill[i].set_color(Color.average_color(colors))
+                    poly_fill[i].set_color(col.average_color(colors))
                     filled.append(poly_fill[i])
                 
-def draw_flight_path(map_points, canvasName):
-    for i in range(1, len(map_points)):
-        canvasName.create_line(map_points[i - 1].x, map_points[i - 1].y, map_points[i].x, map_points[i].y)
+'''draws drone flight path with a black line'''
+def draw_flight_path(map_list, canvasName):
+    for i in range(1, len(map_list)):
+        canvasName.create_line(map_list[i - 1].x, map_list[i - 1].y, map_list[i].x, map_list[i].y)
 
 """tester for making drone path"""
-def create_circle(map_point, radius, canvasName): #center coordinates, radius
+def create_circle(map_point, ftpix, mode, canvasName): #center coordinates, radius
     x = map_point.x
     y = map_point.y
-    canvasName.create_oval(x - radius, y - radius, x + radius, y + radius, fill = map_point.nir_rgb)
+    radius = map_point.confidence * ftpix
+    if mode == 'vis':
+        color = map_point.vis_rgb
+    if mode == 'nir':
+        color = map_point.nir_rgb
+    if mode == 'temp':
+        color = map_point.temp_rgb
+    canvasName.create_oval(x - radius, y - radius, x + radius, y + radius, fill = color)
 
-#test on sophia's pc
-#gps_points = gpsPoint.read_drone_csv(r'C:\Users\Sophia\Documents\Sophia\college\Spring2021 Textbooks\CSE326\vir-atlas-master\vir-atlas-master\Data Files\Feb-26th-2021-05-57PM-Flight-Airdata.csv')
-#stella_points = StellaPoint.make_stella_points(r'C:\Users\Sophia\Documents\Sophia\college\Spring2021 Textbooks\CSE326\vir-atlas-master\vir-atlas-master\Data Files\data.csv')
-
-# Test on Brynn's laptop. This should work for everyone since its realative in the git directory. (I'll try and make this dynamic later where the user can enter the directory)
-# gps_points reads in feb26th flight airdata
-# stella_points reads data.csv
-def main():
-    gps_points = gpsPoint.read_drone_csv(r'vir-atlas-master\Data Files\Feb-26th-2021-05-57PM-Flight-Airdata.csv')
-    stella_points = StellaPoint.make_stella_points(r'vir-atlas-master\Data Files\data.csv')
-
-    stella_points = StellaPoint.get_batch(stella_points, "1.X")
-    map_points,width,height = MapPoint.set_xy(gps_points, stella_points, canvas_size)
-
-    height = round(height/10) * 10
-    width = round(width/10) * 10
-
+def get_map(gps_file, stella_file, canvas_size, resolution, mode):
+    map_list, width, height = MapPoint.init_map_list(gps_file, stella_file, canvas_size)
+    
+    width = round(width/resolution) * resolution
+    height = round(height/resolution) * resolution
+    
     window = tk.Tk()
-    vis_map = tk.Canvas(window, width = width, height = height)
-    nir_map = tk.Canvas(window, width = width, height = height)
-    tmp_map = tk.Canvas(window, width = width, height = height)
-
-    vis_map.pack()
-    nir_map.pack()
-    tmp_map.pack()
+    map = tk.Canvas(window, width = width, height = height)
+    map.pack()
 
     poly_fill = get_poly(height, width)
-    filled = draw_data(map_points, poly_fill, 'vis', resolution, width)
+    filled = draw_data(map_list, poly_fill, mode, resolution, width)
     fill_all(filled, poly_fill, width, height, resolution)
     for t in poly_fill:
-        t.draw(vis_map)
-    draw_flight_path(map_points, vis_map)
+        t.draw(map)
 
-    clear_poly(poly_fill)
-    filled = draw_data(map_points, poly_fill, 'nir', resolution, width)
-    fill_all(filled, poly_fill, width, height, resolution)
-    for t in poly_fill:
-        t.draw(nir_map)
-    draw_flight_path(map_points, nir_map)
-
-    clear_poly(poly_fill)
-    filled = draw_data(map_points, poly_fill, 'tmp', resolution, width)
-    fill_all(filled, poly_fill, width, height, resolution)
-    for t in poly_fill:
-        t.draw(tmp_map)
-    draw_flight_path(map_points, tmp_map)
-
+    draw_flight_path(map_list, map)
     window.mainloop()
+    return map
+
+'''return multiplier that translates feet to pixels
+height in pixels '''
+def feet_to_pix(delta_lat, height):
+    delta_ft = delta_lat * 60 * 6076.12
+    return height / delta_ft
+
+def get_map_alt(gps_file, stella_file, canvas_size, resolution, mode):
+    map_list, width, height, delta_lat = MapPoint.init_map_list(gps_file, stella_file, canvas_size)
+    
+    width = round(width/resolution) * resolution
+    height = round(height/resolution) * resolution
+
+    ftpix = feet_to_pix(delta_lat, height)
+    
+    window = tk.Tk()
+    map = tk.Canvas(window, width = width, height = height)
+    map.pack()
+
+    for m in map_list:
+        create_circle(m, ftpix, mode, map)
+
+    draw_flight_path(map_list, map)
+    window.mainloop()
+    return map
+
+# gps_file = r'vir-atlas-master\Data Files\Feb-26th-2021-05-57PM-Flight-Airdata.csv'
+# stella_file = r'vir-atlas-master\Data Files\data.txt'
+# # map = get_map(gps_file, stella_file, canvas_size, resolution, 'vis')
+# map = get_map_alt(gps_file, stella_file, canvas_size, resolution, 'vis')
+
+# def main():
+#     gps_points = gpsPoint.read_drone_csv(r'vir-atlas-master\Data Files\Feb-26th-2021-05-57PM-Flight-Airdata.csv')
+#     stella_points = StellaPoint.make_stella_points(r'vir-atlas-master\Data Files\data.csv')
+
+#     stella_points = StellaPoint.get_batch(stella_points, "1.X")
+#     map_list,width,height = MapPoint.set_xy(gps_points, stella_points, canvas_size)
+
+        # width = round(width/resolution) * resolution
+        # height = round(height/resolution) * resolution
+    
+
+#     window = tk.Tk()
+#     vis_map = tk.Canvas(window, width = width, height = height)
+#     nir_map = tk.Canvas(window, width = width, height = height)
+#     temp_map = tk.Canvas(window, width = width, height = height)
+
+#     vis_map.pack()
+#     nir_map.pack()
+#     temp_map.pack()
+
+    
+#     clear_poly(poly_fill)
+#     filled = draw_data(map_list, poly_fill, 'nir', resolution, width)
+#     fill_all(filled, poly_fill, width, height, resolution)
+#     for t in poly_fill:
+#         t.draw(nir_map)
+#     draw_flight_path(map_list, nir_map)
+
+#     clear_poly(poly_fill)
+#     filled = draw_data(map_list, poly_fill, 'temp', resolution, width)
+#     fill_all(filled, poly_fill, width, height, resolution)
+#     for t in poly_fill:
+#         t.draw(temp_map)
+#     draw_flight_path(map_list, temp_map)
+
+#     window.mainloop()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()

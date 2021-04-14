669b8f6d5e93dddb9689831e1b4dd4ff721d8a62
tylercharity <tylercharity@cs.nmt.edu>
2021-04-14 16:22:33 -0600

    "Fixed syntax errors in main.py - Brynn"

--- main.py
+++ main.py
@@ -6,17 +6,18 @@
 import tkinter as tk
 import menu_bar as menu
 
+
 # Class creating the base window
 class Root(tk.Tk):
     # Main window constructor
     def __init__(self):
         tk.Tk.__init__(self)
 
-        # Add the menubar to the main window
-        menubar = menu.MenuBar(self)
-        self.config(menu=menubar)
+        # Add the menu_bar to the main window
+        menu_bar = menu.MenuBar(self)
+        self.config(menu=menu_bar)
+
 
 if __name__ == "__main__":
-    root=Root()
+    root = Root()
     root.mainloop()
-

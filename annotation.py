e57beba1a23c9362c625d1a96fdf83463be16dd5
tenise <tenise_stansfield@cs.nmt.edu>
2021-04-14 21:11:00 -0600

    added some bare bones stuff for AnnotationEditor in annotation.py

--- annotation.py
+++ annotation.py
@@ -1,23 +1,48 @@
 # @authors Tenise
 # @date 04/14/2021
 # @brief Handles Annotations
-# @todo :(( everything
+# @todo add "Cancel" button to AnnotationEditor, work on Annotation and AnnotationFrame
 
 import tkinter as tk
+import sys
 
 
-class Annotation:
+class Annotation(object):
     def __init__(self, x_coordinate, y_coordinate, note):
         tk.Tk.__init__(self)
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.note = note
 
-class AnnotationFrame(tk.Frame):
+
+# Responsible for the annotation_frame in main.py
+class AnnotationFrame:
     def __init__(self, master):
-        tk.Frame.__init__(self, master)
+        pass
+        # annotations = self.annotations =
 
-class AnnotationEditor(notes):
-    def __init__(self):
-        self
+    def notes(self, event):
+        pass
 
+
+# Responsible for editing/adding Annotations
+class AnnotationEditor(object):
+    # constructor for AnnotationEditor
+    def __init__(self, master):
+        # call for popup window
+        top = self.top = tk.Toplevel(master)
+
+        # label for Annotation Editor
+        self.label = tk.Label(top, text="VIR Atlas Annotations Editor")
+        self.label.pack()
+
+        # create entry
+        self.note = tk.Entry(top)
+        self.note.pack()
+
+        # create "Save" button that initiates save_note()
+        self.save = tk.Button(top, text="Save", command=self.save_note)
+
+    def save_note(self):
+        self.save = self.note.get()
+        self.top.destroy()

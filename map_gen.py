8df1fa8a6a8722601ae03b493f2e383a4f187e94
Marisa Loraas <marisa_loraas_2000@auth.cs.nmt.edu>
2021-04-22 18:43:05 -0600

    Update to update annotations -Marisa

--- map_gen.py
+++ map_gen.py
@@ -12,6 +12,7 @@ import tkinter as tk
 import map_point
 import color as col
 import math
+import annotation
 
 canvas_size = 1000  # test canvas size
 resolution = 10
@@ -260,6 +261,7 @@ class Map:
         elif gps_file != 0 and stella_file != 0:
             self.map_list, self.width, self.height, self.delta_lat = map_point.init_map_list(gps_file, stella_file,
                                                                                              canvas_size)
+                                                                                             annotation.set_map_list(self.map_list)
             self.scale = feet_to_pix(self.delta_lat, self.height)
 
     def clear_map(self):

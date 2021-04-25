0524972f591614db322235fa81d36624b8925d28
Marisa Loraas <marisa_loraas_2000@auth.cs.nmt.edu>
2021-04-25 10:52:11 -0600

    annotations implemented

--- annotation.py
+++ annotation.py
@@ -11,6 +11,7 @@ import map_gen
 global map_list
 global scale
 global root
+global selected
 
 
 class Annotation(object):
@@ -40,26 +41,29 @@ class AnnotationFrame(tk.Frame):
         # constructor for AnnotationFrame
         tk.Frame.__init__(self, master)
         self.master = master
+        root.annotation_frame = self
         self.width = 420
         self.height = 420
         #for annotations that are not in a map point
         self.annotations = list()
-        # self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg="white")
 
+        # edit and save buttons
+        self.edit_btn = tk.Button(self, text="Edit", bg="white", fg="#007BA7")
+        self.delete_btn = tk.Button(self, text="Delete", bg="white", fg="#007BA7")
+        root.annotation_frame.edit_btn = tk.Button(root.annotation_frame, text="Edit", bg="white", fg="#007BA7")
+        root.annotation_frame.delete_btn = tk.Button(root.annotation_frame, text="Delete", bg="white", fg="#007BA7")
+
+        # self.canvas = tk.Canvas(self, width=self.width, height=self.height, bg="white")
         # set up the Listbox of all annotations
-        self.listbox = tk.Listbox(self, width=70, height=14, bg="white")
-        tk.Label(self, text="Annotations", bg="blue", fg="white", font=("Courier", "16", "bold")).pack()
+        self.listbox = tk.Listbox(self, width=70, height=10, bg="white", selectmode="single", listvariable=self.annotations)
+        tk.Label(self, text="Annotations", bg="#007BA7", fg="white", font=("Courier", "16", "bold")).pack()
         self.listbox.pack()
+        self.listbox.bind('<<ListboxSelect>>', root.annotation_frame.selection)
+
 
         # set up Scrollbar for Listbox
         self.scrollbar = tk.Scrollbar(self)
-        self.scrollbar.pack()
-
-        # insert available annotations into listbox
-        for point in map_list:
-            # check if there's something in the annotation attribute
-            if point.annotation != "":
-                self.listbox.insert("end", point)
+        self.scrollbar.pack(side="right")
 
         # attaching listbox to scrollbar
         self.listbox.config(yscrollcommand=self.scrollbar.set)
@@ -71,15 +75,45 @@ class AnnotationFrame(tk.Frame):
     def add_annotation(self, annotation):
         self.listbox.delete(0, 'end')
         root.annotation_frame.annotations.append(annotation)
-        for point in map_list:
+        for item in root.annotation_frame.annotations:
             # check if there's something in the annotation attribute
-            if point.annotation != "":
-                self.listbox.insert("end", point.annotation)
-            if point.annotation == annotation.note:
-                if annotation in root.annotation_frame.annotations:
-                    root.annotation_frame.annotations.remove(annotation)
-        for annotation in root.annotation_frame.annotations:
-            self.listbox.insert('end', annotation.note)
+            if item.note != "":
+                self.listbox.insert("end", item)
+        root.annotation_frame.listbox = self.listbox
+        self.listbox.pack()
+
+
+    def edit_annotation(self):
+        for item in root.annotation_frame.annotations:
+            if selected == str(item):
+                annotation = AnnotationEditor(item)
+                annotation.note.insert('1.0', item.note)
+
+
+
+    def delete_annotation(self):
+        temp = 0
+        for item in root.annotation_frame.annotations:
+            if selected == str(item):
+                root.annotation_frame.annotations.remove(item)
+                root.annotation_frame.listbox.delete(temp)
+                root.annotation_frame.listbox.pack()
+            temp += 1
+
+
+    def selection(self, evt):
+        event = evt.widget
+        cur = event.curselection()
+        if len(cur) > 0:
+            select = event.curselection()
+            annotation = event.get(select)
+            get_selection(annotation)
+        root.annotation_frame.edit_btn.configure(command=root.annotation_frame.edit_annotation, font=("Courier", "12", "bold italic"))
+        root.annotation_frame.edit_btn.pack(side="left", ipadx=10)
+        root.annotation_frame.delete_btn.configure(command=root.annotation_frame.delete_annotation, font=("Courier", "12", "bold italic"))
+        root.annotation_frame.delete_btn.pack(side='right', ipadx=10)
+
+
 
 # Responsible for editing/adding Annotations
 class AnnotationEditor(Annotation):
@@ -89,24 +123,27 @@ class AnnotationEditor(Annotation):
         self.annotation = new_annotation
         self.top = tk.Toplevel()
         self.top.title("VIR Atlas Annotations Editor")
-        self.top.geometry("400x600")
+        self.top.geometry("400x620")
         # create attribute frame
-        tk.Label(self.top, text="Point Attributes").pack(side="top")
+        title = "Point attributes at x: " + str(new_annotation.x) + " y: " + str(new_annotation.y)
+        tk.Label(self.top, text=title, bg="#007BA7", fg="white", font=("Courier", "12", "bold")).pack(side="top")
         # add attributes to frame
         self.get_attribute(new_annotation.x, new_annotation.y)
 
         # create entry box for input
-        self.note = tk.Text(self.top, width=40, height=10)
+        self.note = tk.Text(self.top, width=40, height=10, font=("Courier", "10", "bold italic"), bg="white", fg="#007BA7")
         self.note.pack()
 
         # create "Save" button that initiates save_note()
-        self.save = tk.Button(self.top, text="Save", command=self.save_note)
+        self.save = tk.Button(self.top, text="Save", bg="white", fg="#007BA7", font=("Courier", "10", "bold italic"), command=self.save_note)
         self.save.pack()
 
         # create "Cancel" button that destroys the window
         # Do we need to make sure that this makes no changes to the Frame? I doubt it
-        self.cancel = tk.Button(self.top, text="Cancel", command=self.cancel)
+        self.cancel = tk.Button(self.top, text="Cancel", bg="white", fg="#007BA7", font=("Courier", "10", "bold italic"), command=self.cancel)
         self.cancel.pack()
+
+        self.top.configure(bg="#007BA7")
 
     # gets and displays all attributes (if available)
     def get_attribute(self, x, y):
@@ -133,14 +170,14 @@ class AnnotationEditor(Annotation):
                     point.stella_point.nir_pows[2]) + "\n 760 nm-> " + str(
                     point.stella_point.vis_pows[3]) + "\n 810 nm->" + str(
                     point.stella_point.vis_pows[4]) + "\n 860 nm-> " + str(point.stella_point.vis_pows[5])
-                tk.Label(self.top, text=surface_temp).pack(side="top")
-                tk.Label(self.top, text=air_temp).pack(side="top")
-                tk.Label(self.top, text=relative_humidity).pack(side="top")
-                tk.Label(self.top, text=air_temp).pack(side="top")
-                tk.Label(self.top, text=air_pressure_hpa).pack(side="top")
-                tk.Label(self.top, text=altitude_m_uncal).pack(side="top")
-                tk.Label(self.top, text=vis_pows).pack(side="top")
-                tk.Label(self.top, text=nir_pows).pack(side="top")
+                tk.Label(self.top, text=surface_temp, bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=air_temp, bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=relative_humidity, bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=air_temp,bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=air_pressure_hpa, bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=altitude_m_uncal, bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=vis_pows,bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
+                tk.Label(self.top, text=nir_pows,bg="#007BA7", fg="white", font=("Courier", "9", "bold")).pack(side="top")
                 flag = 1
                 break
             else:
@@ -151,6 +188,12 @@ class AnnotationEditor(Annotation):
 
     # saves notes made by user to the annotation attribute
     def save_note(self):
+        temp = 0
+        for item in root.annotation_frame.annotations:
+            temp += 1
+            if item is self.annotation:
+                root.annotation_frame.annotations.remove(item)
+                root.annotation_frame.listbox.delete(temp)
         self.annotation = Annotation(self.annotation.x, self.annotation.y, self.note.get('1.0', 'end-1c'))
         root.annotation_frame.add_annotation(self.annotation)
         self.top.destroy()
@@ -168,3 +211,8 @@ def set_map_list(map_points, map_scale):
 def get_root(main_root):
     global root
     root = main_root
+
+
+def get_selection(annotation):
+    global selected
+    selected = annotation
